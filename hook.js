11e6fb60933ca0dc8742af76d7fd9bbe87c8c137
barnettxu <xuxiongfeng@yimian.com.cn>
2023-05-25 17:17:41 +0800

    add hook.js

---
+++ hook.js
@@ -0,0 +1,29 @@
+const frida = require('frida');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+let session, script;
+async function run() {
+    const source = await readFile(path.join(__dirname, '_agent.js'), 'utf8');
+    session = await frida.attach('iTunes');
+    script = await session.createScript(source);
+    script.message.connect(onMessage);
+    await script.load();
+}
+
+run().catch(onError);
+
+function onError(error) {
+    console.error(error.stack);
+}
+
+function onMessage(message, data) {
+    if (message.type === 'send') {
+        console.log(message.payload);
+    } else if (message.type === 'error') {
+        console.error(message.stack);
+    }
+}

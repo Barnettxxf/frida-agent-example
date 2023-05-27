4a0ab629057b77bb0daef099f4895b589f1cb5ba
Barnett Xu <xuxiongfeng@yimian.com.cn>
2023-05-27 19:03:57 +0800

    add hook.ts

---
+++ hook.ts
@@ -0,0 +1,29 @@
+import {attach, getUsbDevice, Device, Message} from 'frida';
+import fs from "fs";
+import path from "path";
+import util from "util";
+
+const readFile = util.promisify(fs.readFile);
+
+function onMessage(message: Message, data: Buffer | null) {
+    if (message.type === 'send') {
+        console.log(message.payload);
+    } else if (message.type === 'error') {
+        console.error(message.stack);
+    }
+}
+
+function onError(error: Error) {
+    console.error(error.stack);
+}
+
+(async () => {
+    const source = await readFile(path.join(__dirname, '_agent.js'), 'utf8');
+
+    const device: Device = await getUsbDevice();
+    const process = await device.getProcess("猿人学2022");
+    const session = await attach(process.pid);
+    const script = await session.createScript(source)
+    script.message.connect(onMessage);
+    await script.load();
+})().catch(onError)

4d6814a1169841461339ab38d5bdce578b1122de
Barnett Xu <xuxiongfeng@yimian.com.cn>
2023-05-28 08:40:33 +0800

    add hook.ts

--- hook.ts
+++ hook.ts
@@ -2,6 +2,7 @@ import {attach, getUsbDevice, Device, Message} from 'frida';
 import fs from "fs";
 import path from "path";
 import util from "util";
+import {print} from "./utils/common";
 
 const readFile = util.promisify(fs.readFile);
 
@@ -19,11 +20,11 @@ function onError(error: Error) {
 
 (async () => {
     const source = await readFile(path.join(__dirname, '_agent.js'), 'utf8');
-
     const device: Device = await getUsbDevice();
     const process = await device.getProcess("猿人学2022");
     const session = await attach(process.pid);
     const script = await session.createScript(source)
     script.message.connect(onMessage);
+    print('[*] Running CTF')
     await script.load();
 })().catch(onError)
---
+++ yuerenxueapp/q1/index.ts
@@ -0,0 +1,28 @@
+Java.perform(() => {
+    const Sign = Java.use('com.yuanrenxue.match2022.security.Sign');
+    console.log('Sign:', Sign)
+    const sign = Sign.sign;
+    sign.implementation = function (a: any) {
+        // let text = "";
+        // for (let i = 0; i < a.length; i++) {
+        //     text += String.fromCharCode(a[i]);
+        // }
+        console.log('params:', a)
+        const result = this.sign(a)
+        console.log('result:', result)
+        return result;
+    };
+    const String = Java.use('java.lang.String');
+    console.log('String:', String);
+    String.format.overload('java.lang.String', '[Ljava.lang.Object;').implementation = function (a: any, b: any) {
+        console.log('a:', a);
+        console.log('b:', b);
+        return String.format(a, b)
+    }
+    String.format.overload('java.util.Locale', 'java.lang.String', '[Ljava.lang.Object;').implementation = function (a: any, b: any, c: any) {
+        console.log('a:', a);
+        console.log('b:', b);
+        console.log('c:', c);
+        return String.format(a, b, c)
+    }
+});
